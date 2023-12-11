f6cc8eb1f4a0b376cbe2a380d95e7ecbb568b722
Wesley Victor <vitorwesley1013m@gmail.com>
2023-12-10 23:17:17 -0300

    update

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.getElementById('check');
+
+  checkbox.addEventListener('change', function () {
+      var headerMenu = document.querySelector('.header-menu');
+      headerMenu.classList.toggle('show-menu', checkbox.checked);
+  });
+});
