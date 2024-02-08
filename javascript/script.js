a9f4dc31e6636877e8564b147d72400d287be98a
Wesley Victor <vitorwesley1013m@gmail.com>
2024-02-08 20:21:58 -0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
     headerMenu.classList.toggle("show-menu", checkbox.checked);
   });
 });
+
+function voltarAoTopo() {
+  window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+  });
+}
+
+function toggleButtonDisplay() {
+  const btnTopo = document.getElementById("btnTopo");
+  const scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (scrollTop > 20) {
+      btnTopo.style.display = "block";
+      btnTopo.style.opacity = "1";
+  } else {
+      btnTopo.style.opacity = "0";
+      setTimeout(() => {
+          if (btnTopo.style.opacity === "0") {
+              btnTopo.style.display = "none";
+          }
+      }, 300);
+  }
+}
+
+window.addEventListener("scroll", toggleButtonDisplay);
